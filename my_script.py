695fe92204fe486c6df326cd3e08c7fd0440b59f
So Yee Cheung <soyeecheung@Sos-MacBook-Pro.local>
2023-07-14 16:10:02 -0400

    initial commit

---
+++ my_script.py
@@ -0,0 +1,5 @@
+# make sure you are in the directory that contains your measurement set (.ms file)
+vis="sis14_twhya_selfcal.ms"
+
+listobs(vis=vis)
+# After the above command you can see the details about the observation in the logger, such as the time on different sources (targets, calibrators, etc.), and info about antennas.
