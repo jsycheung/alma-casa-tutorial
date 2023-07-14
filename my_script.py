8ce091125723d3191e6554fed0c66e2fe0436846
So Yee Cheung <soyeecheung@Sos-MacBook-Pro.local>
2023-07-14 16:52:53 -0400

    added tclean commands

--- my_script.py
+++ my_script.py
@@ -13,9 +13,11 @@ plotms(vis=vis, xaxis='u', yaxis='v', avgchannel='10000', avgspw=False, avgtime=
 export DISPLAY=:1
 
 # After running the above plotms command, you can see the uv plot! This is the visibility. If we do fourier transform on the uv plot, then you will get the sky brightness.
+# In the command, the color axis is 'field', meaning that different colors on the plot indicate different fields (or sources)
 
-#Then we can make the continuum image!
-tclean(vis='sis14_twhya_selfcal.ms',
+# Then we can make the continuum image!
+# We first ran the command without specifying niter, so we only got the warning but the casa viewer won't appear because we're not trying to clean it (niter not specified).
+tclean(vis=vis,
        imagename='phase_cal',
        field='3',
        spw='',
@@ -26,4 +28,22 @@ tclean(vis='sis14_twhya_selfcal.ms',
        cell=['0.1arcsec'],
        weighting='briggs',
        threshold='0.0mJy',
+       interactive=True)
+
+# remove any file that starts with phase_cal
+os.system('rm -rf phase_cal.*')
+
+# Now we re-run the command with niter=5000 so now casa viewer appears because it knows we are trying to interactive clean.
+tclean(vis=vis,
+       imagename='phase_cal',
+       field='3',
+       spw='',
+       specmode='mfs',
+       deconvolver='hogbom',
+       gridder='standard',
+       imsize=[128,128],
+       cell=['0.1arcsec'],
+       weighting='briggs',
+       threshold='0mJy',
+       niter=5000,
        interactive=True)
